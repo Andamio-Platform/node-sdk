7ebc89ee725b3cc59da08c0e44ff1a62e89e4443
nelsonksh <nelsonksh@gimbalabs.io>
2025-03-08 15:32:55 +0530

    support cjs

--- src/indexer/core/utils/get_address.ts
+++ src/indexer/core/utils/get_address.ts
@@ -9,35 +9,33 @@ import { NetworkId } from "~/types";
 import { SdkError } from "~/utils";
 
 export async function getAddress(client: UtxorpcClient, courseNftPolicy: string, filter: InstanceFilter): Promise<string> {
-    const instanceValidator = new Instance(client);
-    try {
-      const instanceUtxos = await instanceValidator.getUtxos(
-        courseNftPolicy,
-        filter,
-      );
+  const instanceValidator = new Instance(client);
+  try {
+    const instanceUtxos = await instanceValidator.getUtxos(
+      courseNftPolicy,
+      filter,
+    );
 
-      console.log("instanceUtxos", instanceUtxos.length);
-
-      if (!instanceUtxos[0].parsedValued?.script?.script.value) {
-        throw new Error("Invalid course NFT UTXO: missing script value");
-      }
-
-      const cborHex = bytesToHex(
-        instanceUtxos[0].parsedValued.script.script.value as Uint8Array,
-      );
-      const doubleEncodedCborHex = cbor
-        .encode(Buffer.from(cborHex, "hex"))
-        .toString("hex");
-
-      const serializedScript = serializePlutusScript(
-        { code: doubleEncodedCborHex, version: "V3" },
-        AndamioConfig.stakingSH,
-        NetworkId[client.network],
-        true,
-      );
-
-      return serializedScript.address;
-    } catch (error) {
-      throw new SdkError(`Failed to initialize course: ${error}`);
+    if (!instanceUtxos[0].parsedValued?.script?.script.value) {
+      throw new Error("Invalid course NFT UTXO: missing script value");
     }
+
+    const cborHex = bytesToHex(
+      instanceUtxos[0].parsedValued.script.script.value as Uint8Array,
+    );
+    const doubleEncodedCborHex = cbor
+      .encode(Buffer.from(cborHex, "hex"))
+      .toString("hex");
+
+    const serializedScript = serializePlutusScript(
+      { code: doubleEncodedCborHex, version: "V3" },
+      AndamioConfig.stakingSH,
+      NetworkId[client.network],
+      true,
+    );
+
+    return serializedScript.address;
+  } catch (error) {
+    throw new SdkError(`Failed to initialize course: ${error}`);
   }
+}
