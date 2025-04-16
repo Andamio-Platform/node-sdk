c7ebd976cd8726ae9868e3dbc12203c4527d9744
nelsonksh <nelsonksh@gimbalabs.io>
2025-04-16 18:09:31 +0530

    init docs

---
+++ docs/content/_meta.js
@@ -0,0 +1,8 @@
+export default {
+    index: 'Getting Started',
+    u5c: 'UTxO RPC',
+    // components: 'Components',
+    provider: 'Provider',
+    tx: 'Transaction Builder',
+    utils: 'Utilities',
+}
