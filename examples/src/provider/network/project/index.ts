2dec85ce28d058eb6451b44036d539ef2f6892e1
nelsonksh <nelsonksh@gimbalabs.io>
2025-07-25 09:51:21 +0530

    remove examples

--- examples/src/provider/network/project/index.ts
+++
@@ -1,7 +0,0 @@
-import AndamioSDK from "@andamiojs/sdk";
-
-export async function Project(sdk: AndamioSDK, projectNftPolicy: string) {
-    const policy = await sdk.provider.network.project.getTreasuryTokenPolicy(projectNftPolicy);
-
-    return policy;
-}
